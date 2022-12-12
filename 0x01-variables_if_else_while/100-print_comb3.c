a9607720494ef3ce4b2caf158f3be3fcd2e97d80
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 <ayodeleojo97@gmail.com>
2022-12-12 23:00:09 +0100

    100 print commit

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/*
+ * main - Entry point
+ *
+ *Description: print all different combination of 2 digits
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int x, y;
+
+	for (x =48; x <= 57; x++)
+	{
+		for (y = 48; y ,= 57; y++)
+		{
+			if (y>x)
+			{
+				putchar(x);
+				putchar(y);
+				if (x != 56 || y != 57)
+				{
+					putchar(',');
+					putchar(32
+
