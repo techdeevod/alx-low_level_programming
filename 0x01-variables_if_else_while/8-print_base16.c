466d07e68c8a9b71ac6d13521fcdf5c22a0a30cf
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 <ayodeleojo97@gmail.com>
2022-12-12 22:38:37 +0100

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/*
+ * main -Entry point
+ *
+ * Return: Always 0 (success)
+ * Decription:print hexadecimal number
+ */
+int main(void)
+{
+	char x = 48;
+
+	for (; x <= 57; x++)
+		putchar(x);
+
+	for (x = 'a'; x <= 'f'; x++)
+		putchar(c);
+	putchar('\n');
+	return (0);
+}
