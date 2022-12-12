d3d26defbe6807764b680d7923419e517711454e
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 <ayodeleojo97@gmail.com>
2022-12-12 22:02:34 +0100

    print alphabet except e and q

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/*
+ * main -Entry point
+ *
+ * Return: Always 0 (success)
+ * */
+int main(void)
+{
+	char x = 'a';
+	
+	for (; x <= 'z'; x++)
+	{
+		if (x == 'e' || a == 'q')
+			continue;
+		putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}	
