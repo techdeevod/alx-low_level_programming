797493fa4c10bb1455c82e2a6be6a5fc0eed794a
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 <ayodeleojo97@gmail.com>
2022-12-12 22:18:59 +0100

    single digit of base 10 starting from 0

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/*
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ * 
+ */
+int main(void)
+{
+	int x = 0;
+
+	for (; x !=10; x++)
+		printf("%d", x);
+	printf("\n");
+	return (0);
+}
