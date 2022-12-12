264920bc9eb4f7fa0a5e17fbd8bc0981e5e0b08f
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 <ayodeleojo97@gmail.com>
2022-12-12 22:27:17 +0100

    6 print numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/*
+ * main -Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int x = 48;
+
+	for (; x <= 57; x++)
+		putchar(d);
+	putchar('\n');
+	return (0);
+}
