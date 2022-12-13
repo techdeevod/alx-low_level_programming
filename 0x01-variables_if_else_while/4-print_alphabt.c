7a3d76ab4f1544805693a80b0ae6ced06a0c7e74
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 <ayodeleojo97@gmail.com>
2022-12-13 05:26:17 +0100

    print alphabets in lowercase except e and q

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
-/*
+/**
  * main -Entry point
  *
  * Return: Always 0 (success)
- * 
  */
 int main(void)
 {
@@ -16,5 +15,5 @@ int main(void)
 		putchar(x);
 	}
 	putchar('\n');
-	return(0);
+	return (0);
 }
