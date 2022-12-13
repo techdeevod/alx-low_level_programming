5a85a48dcf85b6bec29f01a60cb38d4d1a187a4b
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 <ayodeleojo97@gmail.com>
2022-12-13 04:24:27 +0100

    100 print commit

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,16 +10,22 @@ int main(void)
 {
 	int x, y;
 
-	for (x =48; x <= 57; x++)
+	for (x = 48; x <= 57; x++)
 	{
-		for (y = 48; y ,= 57; y++)
+		for (y = 48; y <= 57; y++)
 		{
-			if (y>x)
+			if (y > x)
 			{
 				putchar(x);
 				putchar(y);
 				if (x != 56 || y != 57)
 				{
 					putchar(',');
-					putchar(32
-
+					putchar(32)
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
