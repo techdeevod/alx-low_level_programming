bdc8e8b02565c61ad46487c67ebbc2bd00aa9ef8
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 <ayodeleojo97@gmail.com>
2022-12-13 05:21:21 +0100

    print all single digits of base 10

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-/*
+/**
  * main - Entry point
  *
  * Return: Always 0 (success)
@@ -9,7 +9,7 @@ int main(void)
 {
 	int x = 0;
 
-	for (; x !=10; x++)
+	for (; x != 10; x++)
 		printf("%d", x);
 	printf("\n");
 	return (0);
