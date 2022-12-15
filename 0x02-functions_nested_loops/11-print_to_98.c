b5a86e0ca2012a336f853d1c997d2baf62570169
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 <ayodeleojo97@gmail.com>
2022-12-15 02:18:53 +0100

    print all natural numbers

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,8 +10,8 @@
 void print_to_98(int n)
 {
 	if (n < 98)
-		for (;, n < 98; n++)
-			printf("%d, ", n)
+		for (; n < 98; n++)
+			printf("%d, ", n);
 	else if (n > 98)
 		for (; n > 98; n--)
 			printf("%d, ", n);
