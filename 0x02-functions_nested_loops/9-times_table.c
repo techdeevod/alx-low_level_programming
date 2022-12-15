1815f6edef955afd0a6e2e8a054de103b571ce92
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 <ayodeleojo97@gmail.com>
2022-12-15 02:01:42 +0100

    9 times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * times_table - print the 9 times table, starting with 0.
+ *
+ * Return: void.
+ */
+void times_table(void)
+{
+	int a, b, c;
+	for (a = 0; a < 10; a++)
+	{
+		for (b 0; b < 10; b++)
+		{
+			c = a * b;
+			if ( b == 0)
+				_putchar(p + '0');
+			else
+			{
+				if (c < 10)
+				{
+					_putchar(',');
+					_putchar(32);
+					_putchar((c / 10) + '0');
+					_putchar((c % 10) + '0');
+				}
+			}
+		}
+		_putchar('\n');
+	}
+}
