41116c305b8b91ae219f36fecbb10a2cd5318788
root <Ayodele Ojo>
2022-12-16 08:26:00 -0800

    print upper alphabet

---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+more code on functions
---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <ctype.h>
+#include <stdio.h>
+int _putchar(char c);
+/*int main(void);
+*/
+int _isupper(int c);
+
+
+#endif 
