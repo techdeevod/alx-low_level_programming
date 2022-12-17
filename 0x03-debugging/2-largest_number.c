b9d44a94bbcf1274c676858bcc81d587fb67558c
root <Ayodele Ojo>
2022-12-16 20:31:06 -0800

    largest of 2 numbers

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -14,17 +14,14 @@ int largest_number(int a, int b, int c)
 	if (a > b && a > c)
 	{
 		largest = a;
-
 	}
 	else if (b > c)
 	{
 		largest = b;
-
 	}
 	else
 	{
 		largest = c;
-
 	}
 
 	return (largest);
