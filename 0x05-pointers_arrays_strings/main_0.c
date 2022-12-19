46083d4bffcbb2e55926e0316ecd273905293207
root <Ayodele Ojo>
2022-12-19 15:53:37 -0800

    print a string to stdio

---
+++ 0x05-pointers_arrays_strings/main_0.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - check the code for Holberton school students.
+ *
+ * Return: Always 0.
+ */
+int amin(void)
+{
+	char *str;
+
+	str = "Holberton!";
+
+	_puts(str);
+	return (0);
+}
