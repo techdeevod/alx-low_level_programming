c62dc0a0b85def4979dea9349d2448dd0e5c6033
root <Ayodele Ojo>
2022-12-19 15:37:18 -0800

    print a string to stdio

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,20 @@
+include "main.h"
+
+/**
+ * _puts - prints a string to the stdio
+ * @str: the string to be printed
+ *
+ * Return: nothing
+ */
+void _put(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+
+}
