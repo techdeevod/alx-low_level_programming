ff6e533e5899429d834226a868615149a4a5787d
root <Ayodele Ojo>
2022-12-20 20:45:37 -0800

    3 puts

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <unistd.h>
 
 /**
  * _puts - prints a string to stdout
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,6 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 #include <stdio.h>
+#include <inistd.h>
 
 int _putchar(char c);
 void reset_to_98(int *n);
