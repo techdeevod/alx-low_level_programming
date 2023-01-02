434bcab25de88708b4f8b05f3d611fc8ae40cea6
root <Ayodele Ojo>
2023-01-02 12:02:59 -0800

    search a string for any set if bytes

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -12,16 +12,12 @@
 char *_strpbrk(char *s, char *accept)
 {
 
-	int i;
+	int i, j;
 
-	while (*s)
-	{
-		for (i = 0; accept[i]; i++)
-		{
-			if (*s == accept[i])
-				return (s);
-		}
-		s++;
-	}
-	return (NULL);
+	for (i = 0; s[i] != '\0'; i++)
+		for (j = 0; accept[j] != '\0'; j++)
+			if (s[i] == accept[j])
+				return (s + i);
+
+	return (0);
 }
