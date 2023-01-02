fa12e13f917363f1aca0b11bb7ee4f801eb3f95f
root <Ayodele Ojo>
2023-01-02 10:55:22 -0800

    locate char in a string

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -11,13 +11,9 @@
 char *_strchr(char *s, char c)
 {
 
-	int i;
-	
-	for (i = 0; s[i] != '\0'; i++)
-	{
-		if (s[i] == c)
-		return (&s[i]);
-	}
+	while (*s != c)
+		if (!*s++)
+			return (0);
 
-	return (0);
+	return (s);
 }
