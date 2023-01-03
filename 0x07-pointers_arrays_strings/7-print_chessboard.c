9eda056ab6999044f8384c05b4928a4368cf1b52
root <Ayodele Ojo>
2023-01-03 15:44:42 -0800

    print chessboard

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_chessboard - print the chessboard.
+ * @a: state of the chessboard
+ *
+ * Return: null
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int x, y;
+
+	for (x = 0; x < 8; x++)
+	{
+		for (y = 0; y < 8; y++)
+			_putchar(a[x][y]);
+		_putchar(10);
+	}
+}
