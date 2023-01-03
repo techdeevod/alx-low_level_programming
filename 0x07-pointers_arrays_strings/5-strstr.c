64c52fe2e42b1fcde5f41dc0e641be3d290c3bb1
root <Ayodele Ojo>
2023-01-03 15:13:26 -0800

    find first occurrence of the substring

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * _strstr - function that finds the first occurrence of a substring
+ * @needle: substing
+ * @haystack: string
+ *
+ * Return: a pointer to the beginning of the located substring or NULL
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+
+	char *hay, *need;
+
+	while (*haystack)
+	{
+
+		hay = haystack;
+		need = needle;
+
+		while (*haystack && *pattern && *haystack == *pattern)
+		{
+			haystack++;
+			pattern++;
+		}
+
+		if (!*pattern)
+			return (hay);
+
+		haystack = hay + 1;
+	}
+	return (0);
+}
