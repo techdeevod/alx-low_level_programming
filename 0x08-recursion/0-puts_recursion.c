f78d24fb4750f001c6035800bfcc4cfbb3093555
root <Ayodele Ojo>
2023-01-05 10:27:15 -0800

    prints a string

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _put_recursion - print a string followed by a new line
+ * @s: the strign to print
+ *
+ * Return: Nothing
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+
+	_putchar(*s);
+	s++;
+	_puts_recursion(s);
+}
--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 this directory is for debugging errors
 Introduction to pointers and arrays
 What are pointers and hw to use them. What are arrays and how to use them
+This Directory is for Recursion project
