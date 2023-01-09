ceeb2737bc06c0886b3721f16e6ccb43d1ac21c6
root <Ayodele Ojo>
2023-01-08 16:04:00 -0800

    multiple two numbers

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * mul.c - prints multiplication of two numbers
+ * @argc: int type
+ * @argv: char type
+ * @x: first number
+ * @y: second number
+ * Return: value and 1 if error
+ */
+
+int main(int argc, char *argv[])
+{
+
+	int x = 0, y = 0;
+
+	if (argc >= 3)
+	{
+		x = atoi(argv[1]);
+
+		y = atoi(argv[2]);
+
+		printf("%d\n", x * y);
+	}
+
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	return (0);
+}
