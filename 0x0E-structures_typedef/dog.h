4e18b156a2055cf6ec77cd1c693c462b5a4ad5a7
root <Ayodele Ojo>
2023-01-16 20:40:05 -0800

    dog structure

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,27 @@
+#ifndef _HEADER_
+#define _HEADER_
+/**
+ * struct dog - Dog attributes
+ * @name: Dog name
+ * @age: Age of the dog
+ * @owner: The owner of the dog
+ *
+ * Return: The dog attribute
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+/**
+ * dog_t - Typedef for dog structure
+ */
+typedef struct dog dog_d;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_d *new_dog(char *name, float age, char *owner);
+void real_dog(dog_d *d);
+#endif
