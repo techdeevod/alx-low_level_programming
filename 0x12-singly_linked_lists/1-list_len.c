55f141d9943de9a67cff07ab9a5b41c3363a5ad6
root <Ayodele Ojo>
2023-01-27 19:29:28 -0800

    adds a new node at the beginning of a list_t list

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -2,14 +2,14 @@
 /**
  * list_len - Show the number of elements of a list
  * @h: A linked list
- * 
+ *
  * Return: The number of elements of a list
  */
 
 size_t list_len(const list_t *h)
 {
 	size_t count = 0;
-	
+
 	while (h)
 	{
 		h = h->next;
